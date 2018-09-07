ec2419496dd15f2cfcd75776044bf74b2371b8c2
Tom Hodder <tom@limepepper.co.uk>
2018-09-07 20:53:33 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,137 @@
+Role Name
+=========
+
+A role to help with installing zabbix server, and to provide modules for
+managing users, groups and hosts in Zabbix
+
+Example Playbook
+----------------
+
+If you just want to manage users on an existing zabbix installation, you
+can do a basic role import, and then use the `zabbix_user` module in your own
+roles/playbooks like so;
+
+```yml
+
+tasks:
+  - import_role:
+      name: limepepper.zabbix-server
+
+  - name: create a zabbix user, set password, add email notifications
+    zabbix_user:
+      login_user: Admin
+      login_password: "{{ zabbix_web_admin_pass }}"
+      server_url: http://localhost/zabbix
+      alias: "new_user_1"
+      enabled: yes
+      passwd: xxxxxxx
+      email: xfexxx@example.com
+      name: Marty
+      surname: McFly
+
+```
+In the previous example, the user would be configured to receive all problem
+report notifications
+
+If you want to install, and configure zabbix on the server, you can tell ansible
+to install the server side components.  Currently this role only supports the
+mysql and apache options. (postgresql and nginx are on the todo list)
+
+Zabbix with a mysql backend depends on
+a running mysql and apache services, so something like this should build the
+whole thing;
+
+```yml
+
+tasks:
+  - include_role:
+      name: limepepper.mysql
+
+  - include_role:
+      name: limepepper.apache
+
+  - import_role:
+      name: limepepper.zabbix-server
+    vars:
+      zabbix_server_components:
+        - zabbix-server-mysql
+        - zabbix-web
+
+```
+
+or you can use the classic role import syntax, which is slightly less verbose;
+
+```yml
+- hosts: zabbix
+  roles:
+    - limepepper.mysql
+    - limepepper.apache
+    - limepepper.zabbix-server
+  vars:
+    zabbix_server_components:
+      - zabbix-server-mysql
+      - zabbix-web
+    # use a more recent version of mysql
+    mysql_profile: mysql_community_server_5_7
+
+```
+
+To successfully send email notifications, zabbix needs to know how to send email
+via SMTP.
+
+```yml
+
+- hosts: zabbix
+  roles:
+    - limepepper.mysql
+    - limepepper.apache
+    - limepepper.zabbix-server
+  vars:
+    # use a more recent version of mysql
+    mysql_profile: mysql_community_server_5_7
+    # install mysql backend, and web components
+    zabbix_server_components:
+      - zabbix-server-mysql
+      - zabbix-web
+
+  tasks:
+
+    - name: setup SMTP for the default Email media type
+      zabbix_media:
+        # zabbix api login details
+        login_user: Admin
+        login_password: "{{ zabbix_web_admin_pass }}"
+        server_url: http://localhost/zabbix
+        # module args
+        name: Email
+        type: Email
+        smtp_server: smtp.google.com
+        smtp_server_port: 25
+        from_address: "zabbix@{{ ansible_fqdn }}"
+        auth:
+          username: my_smtp_login
+          password: my_smtp_password
+
+    - name: create a zabbix use and setup email notifications
+      zabbix_user:
+        # zabbix api login details
+        login_user: Admin
+        login_password: "{{ zabbix_web_admin_pass }}"
+        server_url: http://localhost/zabbix
+        # module args
+        alias: "my_notification_user"
+        passwd: xxxxxxx
+        email: xfexxx@example.com
+
+```
+
+License
+-------
+
+BSD
+
+Author Information
+------------------
+
+An optional section for the role authors to include contact information, or a
+website (HTML is not allowed).
---
+++ module_utils/zabbix2.py
@@ -0,0 +1,290 @@
+
+import json
+import os
+import urllib
+import random
+import string
+from ansible.module_utils.urls import fetch_url
+from ansible.module_utils._text import to_text
+from ansible.module_utils.basic import env_fallback
+from ansible.module_utils import crypto as crypto_utils
+
+# in skeleton
+
+
+try:
+    from zabbix_api import ZabbixAPI, ZabbixAPISubClass
+
+    # Extend the ZabbixAPI
+    # Since the zabbix-api python module too old (version 1.0, no higher version so far),
+    # it does not support the 'hostinterface' api calls,
+    # so we have to inherit the ZabbixAPI class to add 'hostinterface' support.
+    class ZabbixAPIExtends(ZabbixAPI):
+        hostinterface = None
+
+        def __init__(self, server, timeout, user, passwd, validate_certs, **kwargs):
+            ZabbixAPI.__init__(self, server, timeout=timeout, user=user,
+                               passwd=passwd, validate_certs=validate_certs)
+            self.hostinterface = ZabbixAPISubClass(
+                self, dict({"prefix": "hostinterface"}, **kwargs))
+
+    HAS_ZABBIX_API = True
+except ImportError:
+    HAS_ZABBIX_API = False
+    raise
+
+
+class ZabbixApi2:
+    def __init__(self,
+                 server_url,
+                 timeout,
+                 http_user,
+                 http_passwd,
+                 validate_certs,
+                 login_user,
+                 login_password):
+
+        self.login_user = login_user
+        self.login_password = login_password
+        self.warnings = []
+        try:
+            self._zapi = ZabbixAPIExtends(server_url, timeout,
+                                          http_user,
+                                          http_passwd,
+                                          validate_certs)
+
+            self._zapi.login(login_user, login_password)
+        except Exception:
+            raise
+            # raise ValueError('A very specific bad thing happened')
+            # # module.fail_json(msg="Failed to connect to Zabbix server: %s" % e)
+
+    def get_apiinfo(self):
+        apiinfos = self._zapi.apiinfo.version([])
+
+        return False, apiinfos, self.warnings
+
+    def user_delete(self, alias):
+        userids = self._zapi.apiinfo.version({'filter': {'alias': alias}})
+        if len(userids) == 1:
+            self._zapi.user.delete([int(userids[0]['userid'])])
+
+        return True, None, self.warnings
+
+    def user_create(self, user_args):
+        self.warnings = []
+
+        # if no passwd provided, generate a random long string
+        if not 'passwd' in user_args or user_args['passwd'] is None:
+            user_args['passwd'] = ''.join(random.choice(
+                string.ascii_uppercase + string.digits) for _ in range(28))
+
+        # if no groups provided, add the user to the Guests group
+        if not 'user_groups' in user_args or not user_args['user_groups']:
+            user_args['user_groups'] = ['Guests']
+
+        grps = self._zapi.usergroup.get(
+            {'filter': {'name': user_args['user_groups']}})
+        grps = [dict(usrgrpid=i['usrgrpid'])
+                for i in grps]
+
+        # zbx_user = self.get_user_by_id(user)
+        parameters = {'alias': user_args['alias'],
+                      'passwd': user_args.get('passwd'),
+                      'usrgrps': grps}
+        self.warn("parameters are %s" % parameters)
+
+        for k, v in user_args.items():
+            if k in ['lang', 'name', 'surname', 'theme', 'url', 'autologin',
+                     'autologout', 'refresh', 'rows_per_page', 'type']:
+                parameters[k] = v
+            elif k == 'email':
+                parameters['user_medias'] = [
+                    {
+                        "mediatypeid": "1",
+                        "sendto": v,
+                        "active": 0,
+                        "severity": 63,
+                        "period": "1-7,00:00-24:00"
+                    }
+                ]
+
+        user_list = self._zapi.user.create(parameters)
+
+        self.warn("user list is %s" % user_list)
+        if len(user_list) > 1:
+            raise ValueError("more than 1 user created. (%s)" % user_list)
+
+        user = self.get_user_by_id(user_list['userids'][0])
+        # user = None
+        return True, user, self.warnings
+
+    def update_user(self, user, user_args):
+        self.warnings = []
+
+        if isinstance(user, (int)):
+            user = self.get_user_by_id(user)
+
+        updated = False
+        for k, v in user_args.items():
+            if k == 'passwd':
+                updated = updated or self.update_user_passwd(
+                    user['userid'], user_args)
+            elif k in ['userid', 'alias']:
+                pass  # can't change these values
+            elif k in ['lang', 'name', 'surname', 'theme', 'url']:
+                if user_args[k] != user[k]:   # compare string values
+                    updated = updated or self.user_update_attribute(
+                        user['userid'], k, v)
+            elif k in ['autologin', 'autologout', 'refresh', 'rows_per_page', 'type']:
+                if int(user_args[k]) != int(user[k]):   # compare string values
+                    updated = updated or self.user_update_attribute(
+                        user['userid'], k, v)
+            elif k in ['enabled']:
+                if v:
+                    self.remove_userid_from_groupname(
+                        user['userid'], 'Disabled')
+                else:
+                    self.add_userid_to_groupname(user['userid'], 'Disabled')
+            elif k == 'email':
+                updated = updated or self.user_update_media(
+                        user['userid'], v)
+
+
+        user = self.get_user_by_id(user['userid'])
+        return updated, user, self.warnings
+
+    def get_user_by_alias(self, alias):
+        result = self._zapi.user.get({'filter': {'alias': alias}})
+        return result[0] if len(result) == 1 else None
+
+    def get_users(self):
+        result = self._zapi.user.get({})
+        return result[0] if len(result) == 1 else result
+
+    def get_user_by_id(self, userid):
+        result = self._zapi.user.get({'userids': userid})
+        return result[0] if len(result) == 1 else None
+
+    def get_mediaby_name_type(self, name, type):
+        result = self._zapi.usermedia.get([])
+        return result[0] if len(result) == 1 else result
+
+    def media_update2(self):
+
+        return True, None, self.warnings
+
+
+
+
+    def user_update_media(self, userid, email):
+        user = self._zapi.user.get(
+            {'userids': userid, 'selectMedias': 'extend'})
+
+        try:
+          if len(user) == 1 and len(user[0]['medias']) == 1:
+            medias = user[0]['medias'][0]
+            if medias['sendto'] != email:
+              medias.pop('mediaid', None)
+              medias.pop('userid', None)
+              medias['sendto'] = email
+              self._zapi.user.updatemedia(dict(users=[{'userid': userid}],
+              medias=medias))
+            else:
+              self.warn("media sendto == email (%s,%s)" % (email, medias['sendto']))
+        except Exception as e:
+            self.warn("user with medias is %s : %s" % (medias, e))
+            raise
+
+
+        # self.warn("user with medias is %s" % user)
+        # self._zapi.user.update(
+        #     [{'userid': userid, 'passwd': user_args['passwd']}])
+        # can't know whether passwd was updated
+        return False
+
+    def update_user_passwd(self, userid, user_args):
+        self._zapi.user.update(
+            [{'userid': userid, 'passwd': user_args['passwd']}])
+        # can't know whether passwd was updated
+        return False
+
+    def user_update_attribute(self, userid, key, val):
+        self._zapi.user.update(
+            [{'userid': userid, key: val}])
+        return True
+
+    def user_get_groups(self, userid):
+        result = self._zapi.usergroup.get({'userids': [userid]})
+        return result
+
+    def remove_userid_from_groupname(self, userid, groupname):
+        dis_grpid = self.get_group_by_groupname(groupname)
+        if not dis_grpid:
+            raise ValueError("The group doesn't exist (%s)" % groupname)
+        grps = self.user_get_groups(userid)
+        if not any(dis_grpid['usrgrpid'] == d['usrgrpid'] for d in grps):
+            self.warn("userid %s not found in group %s" %
+                      (userid, dis_grpid['name']))
+            return False
+        grps = [dict(usrgrpid=i['usrgrpid'])
+                for i in grps if i['usrgrpid'] != dis_grpid['usrgrpid']]
+        if len(grps) == 0:
+            self.warn("Cannot remove user from all groups,"
+                      " current groups (%s)" % [dict(name=i['name'])
+                                                for i in grps])
+            return False
+        parameters = {'userid': userid,
+                      'usrgrps': grps}
+        self.warn("parameters is %s" % parameters)
+        user_list = self._zapi.user.update(parameters)
+        return True
+
+    def add_userid_to_groupname(self, userid, groupname):
+        grp = self.get_group_by_groupname(groupname)
+        if not grp:
+            raise ValueError("The group doesn't exist (%s)" % groupname)
+        grps = self.user_get_groups(userid)
+        if any(grp['usrgrpid'] == d['usrgrpid'] for d in grps):
+            self.warn("userid %s is already found in group %s" %
+                      (userid, grp['usrgrpid']))
+            return False
+        grps.append(grp)
+        grps = [dict(usrgrpid=i['usrgrpid'])
+                for i in grps]
+
+        parameters = {'userid': userid,
+                      'usrgrps': grps}
+        self.warn("parameters is %s" % parameters)
+        user_list = self._zapi.user.update(parameters)
+        return True
+
+    def get_group_by_groupname(self, groupname):
+        result = self._zapi.usergroup.get({'filter': {'name': groupname}})
+        return result[0]
+
+    def warn(self, msg):
+        self.warnings.append(msg)
+
+
+class ZabbixBase:
+    def __init__(self, module, zbx):
+        pass
+
+
+class User(ZabbixBase):
+    def __init__(self, module, zbx):
+
+        pass
+
+
+class UserGroup(ZabbixBase):
+    def __init__(self, module, zbx):
+
+        pass
+
+
+class Media(ZabbixBase):
+    def __init__(self, module, zbx):
+
+        pass
