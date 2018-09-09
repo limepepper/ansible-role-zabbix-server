3adbe7dfa13081f6c944260dc1c9078ee7d54670
Tom Hodder <tom@limepepper.co.uk>
2018-09-09 15:31:40 +0100

    fix for ubuntu18 docker image dpkg filtering

--- action_plugins/zabbix_user.py
+++ action_plugins/zabbix_user.py
@@ -16,14 +16,14 @@ class ActionModule(ActionBase):
         args = self._task.args.copy()
 
         # if "zabbix_server_auth" in task_vars:
-        # if not args.get('login_user'):
-        args['login_user'] = task_vars.get('zabbix_login_user')
+        if not args.get('login_user'):
+          args['login_user'] = task_vars.get('zabbix_login_user')
 
-        # if not args.get('login_password'):
-        args['login_password'] = task_vars.get('zabbix_login_password')
+        if not args.get('login_password'):
+          args['login_password'] = task_vars.get('zabbix_login_password')
 
-        # if not args.get('server_url'):
-        args['server_url'] = task_vars.get('zabbix_server_url')
+        if not args.get('server_url'):
+          args['server_url'] = task_vars.get('zabbix_server_url')
 
         if not result.get('skipped'):
 
